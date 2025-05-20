a7cbf210d4feac072168b61a8cb54fda507a3eae
Zigit Zo <byte@pen.guru>
2025-05-20 15:14:52 +0800

    feat: dedup with linux-firmware

--- hay.sh
+++ hay.sh
@@ -8,8 +8,6 @@ declare -A COPIES=(
   ["qcom/x1e80100/ASUSTeK/vivobook-s15/cdsp_dtbs.elf"]="Driver/QualcommBSP/Subsystem/qcsubsys_ext_cdsp8380_1016_sign/cdsp_dtbs.elf"
   ["qcom/x1e80100/ASUSTeK/vivobook-s15/qccdsp8380.mbn"]="Driver/QualcommBSP/Subsystem/qcsubsys_ext_cdsp8380_1016_sign/qccdsp8380.mbn"
   ["qcom/x1e80100/ASUSTeK/vivobook-s15/qcdxkmsuc8380.mbn"]="Driver/QualcommBSP/GFX/qcdx8380_7500/qcdxkmsuc8380.mbn"
-  ["qca/hmtbtfw20.tlv"]="Driver/QualcommBSP/WIFI_BT/qcbluetooth8380/hmtbtfw20.tlv"
-  ["qca/hmtnv20.b112"]="Driver/QualcommBSP/WIFI_BT/qcbluetooth8380/hmtnv20.b112"
 )
 
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
